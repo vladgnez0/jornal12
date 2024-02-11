fe3a4f834c0c71670ca95bc68149613da9d2fc74
vladgnez0 <114350660+vladgnez0@users.noreply.github.com>
2024-02-11 13:00:12 +0300

    Наше первое веб программирование

---
+++ src/main/java/ru/gnezdilov/jornal12/Jornal12Application.java
@@ -0,0 +1,13 @@
+package ru.gnezdilov.jornal12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jornal12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jornal12Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gnezdilov/jornal12/Jornal12ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gnezdilov.jornal12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jornal12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
